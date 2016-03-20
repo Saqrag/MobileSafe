7827d5a82d6d82982c93927bbc4010454a7fd051
Saqrag <saqrag@outlook.com>
2016-03-20 14:27:44 +0800

    add test

---
+++ src/main/java/com/bug/mobilesafe/test.java
@@ -0,0 +1,15 @@
+package com.bug.mobilesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by saqra on 2016/3/20.
+ */
+public class test extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.err.println("那么");
+    }
+}
